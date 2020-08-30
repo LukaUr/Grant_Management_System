3b22e77ace512d52a1c84cc7a166d9c643970221
LukaUr <lukau@o2.pl>
2020-08-31 00:20:19 +0200

    registration and showAllUsers views updated

--- src/main/java/lukaUr/grantManagementSystem/app/web/users/UserManagementController.java
+++ src/main/java/lukaUr/grantManagementSystem/app/web/users/UserManagementController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Set;
 
 @Controller
 @RequestMapping("/user")
